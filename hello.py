334a0d838cd446230e49905c3c3562eac05aebf7
CBBell99 <chrisbeckertonbell@gmail.com>
2023-11-30 22:31:58 -0800

    Update .gitignore and add new templates

--- hello.py
+++ hello.py
@@ -1,22 +1,36 @@
 from flask import Flask, render_template
-#to run, 'source virt/bin/activate'
-#flask run
+# to run, 'source virt/bin/activate'
+# flask run
 
-#Create a flask instance
+# Create a flask instance
 
 app = Flask(__name__)
 
 # Create a route decorator
-@app.route('/')
 
+
+@app.route('/')
 # def index():
-  # return "<h1>Hello World!</h1>"
+# return "<h1>Hello World!</h1>"
+
+# FILTERS!
+# safe
+# capitalize
+# upper
+# title
+# trim
+# striptags
+
 
 def index():
-  return render_template('index.html')
+    first_name = 'John'
+    stuff = 'This is bold text'
+    favorite_pizza = ["Pepperoni", "cheese", "Mushrooms", 42]
+    return render_template('index.html', first_name=first_name, stuff=stuff, favorite_pizza=favorite_pizza)
 
 # localhost:5000/user/john
-@app.route('/user/<name>')
 
+
+@app.route('/user/<name>')
 def user(name):
-  return "<h1>Hello {}</h1>".format(name)
+    return render_template('user.html', user_name=name)
